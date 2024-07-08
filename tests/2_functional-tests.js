6f660e68986559cbf9bb173c7e1f384b06977add
Laviel Dela Cruz <fafansin@gmail.com>
2024-07-08 07:49:32 +0000

    feat: added unit tests on api requests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,8 +16,8 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -26,10 +26,10 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .keepOpen()
-        .get('/hello?name=xy_z')
+        .get('/hello?name=laviel')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello laviel');
           done();
         });
     });
@@ -39,18 +39,29 @@ suite('Functional Tests', function () {
         .request(server)
         .keepOpen()
         .put('/travellers')
-
+        .send({surname:"Colombo"})
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200, 'Response should be 200');
+          assert.equal(res.type, 'application/json', 'Response should be on type application/json');
+          assert.equal(res.body.name, 'Cristoforo', 'res.body.name should be Cristoforo');
+          assert.equal(res.body.surname, 'Colombo', 'res.body.surname should be Colombo');
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai
+        .request(server)
+        .keepOpen()
+        .put('/travellers')
+        .send({surname: 'da Verrazzano'})
+        .end(function( err, res){
+          assert.equal(res.status, 200, 'response should have status of 200');
+          assert.equal(res.type, 'application/json', 'result should have type of application/json');
+          assert.equal(res.body.name, 'Giovanni', 'res.body.name should be Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano', 'res.body.surname should be da Verrazzano');
+          done();
+        })
     });
   });
 });
